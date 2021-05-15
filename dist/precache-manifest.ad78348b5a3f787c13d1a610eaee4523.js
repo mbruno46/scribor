319d86c3f015e3fe47f68cad4bcc7022092199a4
mbruno46 <mbruno46@users.noreply.github.com>
2021-05-15 09:34:09 +0000

    [websmith.js] publishing website

---
+++ dist/precache-manifest.ad78348b5a3f787c13d1a610eaee4523.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2aee37f1a0608e980a82e1495c42977",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "fba52f1de61a6891c200",
+    "url": "css/app.8f50ac2f.css"
+  },
+  {
+    "revision": "c2c0ef67c48d9a81db281ce46a2e02be",
+    "url": "icons/128x128.png"
+  },
+  {
+    "revision": "149a01cc7176d8f5353d16b7ad0b9987",
+    "url": "icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "46f325f97d866e9870e41af6c5ac6cb9",
+    "url": "icons/apple-touch-icon-192x192.png"
+  },
+  {
+    "revision": "786e2a09bb84bd1338f2029feb359192",
+    "url": "icons/favicon-16x16.png"
+  },
+  {
+    "revision": "908f1583b6528aa94446e2c92eacaa1b",
+    "url": "icons/favicon-32x32.png"
+  },
+  {
+    "revision": "976be5a2ac939254c24c2dd27ba561d3",
+    "url": "img/logo.976be5a2.png"
+  },
+  {
+    "revision": "38d5a528c2d3249085ee31faa6086b27",
+    "url": "index.html"
+  },
+  {
+    "revision": "fba52f1de61a6891c200",
+    "url": "js/app.1bc5c886.js"
+  },
+  {
+    "revision": "48aa6a7173ea6b802230",
+    "url": "js/chunk-vendors.bf42242b.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
